07cb1f68e781b32076df1a99e96256342872de4b
wongkokseng <1221206470@student.mmu.edu.my>
2024-07-01 22:18:10 +0800

    .

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php include ('server.php')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Account</title>
+    <link rel="stylesheet" href="login.css">
+    <script>
+        function openmainpage() {
+            window.location.href='MainPage.html';
+        }
+        function openregister(){
+            window.location.href='register.html';
+        }
+    </script>
+</head>
+<body>
+    <div class="background">
+        <div class="shape"><img src="https://img.freepik.com/premium-vector/food-delivery-calligraphy-hand-lettering-handwritten-logotype-takeaway-service-vector-template-typography-poster-logo-design-banner-flyer-tag-etc_656810-2806.jpg" style="height: 200px;"></div>
+        <div class="shape"><img src="https://th.bing.com/th/id/OIP.IB8GuX3D2m1QO8Kt-4AKAgHaHa?w=1920&h=1920&rs=1&pid=ImgDetMain" style="height: 200px;"></div>
+    </div>
+    <form>
+        <?php include("errors.php")?>
+        <h3 style="font-family: 'Times New Roman', Times, serif;">Login Here</h3>
+
+        <label for="username" style="font-family: 'Times New Roman', Times, serif;">Username</label>
+        <input type="text" placeholder="Your name" name="username">
+
+        <label for="password" style="font-family: 'Times New Roman', Times, serif;">Password</label>
+        <input type="password" placeholder="Password" name="password">
+
+        <button style="font-family: 'Times New Roman', Times, serif;" type="submit" name="login_user">Log In</button>
+        <div class="social">
+          <div class="go" onclick="openmainpage()"><i class="fab fa-google"></i> Main Page</div>
+          <div class="fb" onclick="openregister()"><i class="fab fa-facebook"></i> Register</div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$staff_username = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'accounts');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($password_2)) { array_push($errors, "Please write the password again to confirm password"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM customer WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO customer (username, password) 
+  			  VALUES('$username', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM customer WHERE username='$username' AND password='$password' AND status='active'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination.");
+  	}
+  }
+}
+
+// LOGIN STAFF
+if (isset($_POST['login_staff'])) {
+  $staff_username = mysqli_real_escape_string($db, $_POST['staff_username']);
+  $staff_password = mysqli_real_escape_string($db, $_POST['staff_password']);
+
+  if (empty($staff_username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($staff_password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$staff_password = md5($staff_password);
+  	$query = "SELECT * FROM staff WHERE staff_username='$staff_username' AND staff_password='$staff_password' AND staff_status='active'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $staff_username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: admin.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+
+?>
+
