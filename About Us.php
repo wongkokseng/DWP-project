84bf37d725931ac4459a6899d277ea0f4f86da2c
wongkokseng <1221206470@student.mmu.edu.my>
2024-07-03 14:41:05 +0800

    .

--- About Us.php
+++ About Us.php
@@ -1,3 +1,11 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,11 +16,32 @@
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <script>
         function openhomepage() {
-            window.location.href='index.html';
+            window.location.href='index.php';
         }
     </script>
 </head>
 <body>
+    <div class="navbar">
+        <div class="navbar-brand">Flash Food Delivery</div>
+        <div class="navbar-links">
+            <a href="index.php">Home Page</a>
+            <a href="About Us.php">About Us</a>
+            <a href="Contact Us.php">Contact Us</a>
+            <div class="dropdown">
+                <button class="dropbtn">
+                    <img src="images/default profile.png" alt="Profile Picture">
+                    <?php echo $_SESSION['username']; ?>
+                </button>
+                <div class="dropdown-content">
+                    <a href="profile.php">View Profile</a>
+                    <form method="POST" action="logout.php" style="margin: 0;">
+                        <input type="hidden" name="action" value="logout">
+                        <button type="submit" class="logout-button">Logout</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <section class="about">
         <div class="main">
             <div class="about-text">
