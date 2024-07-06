fcf7fe01a328856d41d3af338fdfa227b923c05c
wongkokseng <1221206470@student.mmu.edu.my>
2024-07-06 11:20:31 +0800

    。

--- server.php
+++ server.php
@@ -51,7 +51,6 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-    $password = md5($password_1);//encrypt the password before saving in the database
 
     $query = "INSERT INTO customer (username, phone_number, password) 
               VALUES('$username', '$phone_number', '$password')";
@@ -89,7 +88,6 @@ if (isset($_POST['login_user'])) {
   }
 
   if (count($errors) == 0) {
-    $password = md5($password);
     $query = "SELECT * FROM customer WHERE username='$username' AND password='$password' AND status='active'";
     $results = mysqli_query($db, $query);
 
